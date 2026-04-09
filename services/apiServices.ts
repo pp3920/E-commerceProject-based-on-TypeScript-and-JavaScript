a5956f66196cd04a39db31ec7a5ac25d2d378a9e
Priyanka22324 <priyanka22324@gmail.com>
2026-04-09 14:38:31 -0400

    adding service class

--- services/apiServices.ts
+++ services/apiServices.ts
@@ -1,20 +1,26 @@
 import { errorHandler } from "../utils/errorHandler.js";
 
-async function fetchURL() {
+/*
+ * Fetching data from the URL and using try/catch block to handle errors 
+ */
+export async function fetchURL() {
     try {
         const URL = "https://dummyjson.com/products/";
         let res = await fetch(URL);
 
         if (!res.ok) {
-            throw new errorHandler("Network response was not ok");
+            throw new Error("fetch is not working");
         }
 
-        const data = await res.json();
-        console.log(data);
+        const data = await res.json()
+        return data;  //returning data
     }
     catch (error) {
         if (error instanceof errorHandler) {
-            console.error(" Error Message:", error.message);
+            throw new errorHandler ("Message:" +  error.message); // error message from Handler class
         } 
+        else{
+             throw new Error ("Unknown Error");
+        }
     }
 }
